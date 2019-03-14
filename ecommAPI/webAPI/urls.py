2d8135c03b6a7734663baf4ef2aa41d6b0dfd184
AL3XAND3R98 <alexander@alexanders-mbp.wkstn.qmul.ac.uk>
2019-03-14 19:07:06 +0000

    Updated endpoint URLs

--- ecommAPI/webAPI/urls.py
+++ ecommAPI/webAPI/urls.py
@@ -4,13 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('setCookie/', views.setCookie, name='setCookie'),
-    path('listProducts/', views.listProducts, name='listProducts'),
-    path('isValid/', views.isValidCookie, name='isValid'),
-    path('availability/<str:pid>/', views.getProduct, name='getProduct'),
+    path('api/setCookie/', views.setCookie, name='setCookie'),
+    path('api/listProducts/', views.listProducts, name='listProducts'),
+    path('api/isValid/', views.isValidCookie, name='isValid'),
+    path('/api/availability/<str:pid>/', views.getProduct, name='getProduct'),
     path('product/<str:pid>/', views.showProduct, name='productPage'),
-    path('addToCart/', views.addToCart, name='addToCart'),
-    path('getCart/<str:basketID>/', views.getCart, name='getCart'),
+    path('api/addToCart/', views.addToCart, name='addToCart'),
+    path('api/getCart/<str:basketID>/', views.getCart, name='getCart'),
     path('showCart/<str:basketID>/', views.showCart, name='showCart'),
 
 ]
